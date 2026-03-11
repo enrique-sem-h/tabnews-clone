ee8980fe34ab14f0d9715ee85f57507b6f05fa66
enrique-sem-h <carvalhoenrique33@gmail.com>
2026-03-11 16:57:35 -0300

    modified the main phrase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Mãezinha, eu te amo e te admiro muito, obrigado por ser quem a senhora é ❤️</h1>
+    return <h1>Mãezinha, eu te amo e te admiro muito, obrigado por ser quem a senhora é ❤️❤️</h1>
 }
 
 export default Home;
