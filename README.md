609caf35fbbc6cca9acf7e23deb74414a8a9d3c1
enrique-sem-h <carvalhoenrique33@gmail.com>
2026-03-16 17:57:40 -0300

    added scripts `lint:check` & `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tabnews-clone
+
 Implementação de um clone do https://www.tabnews.com.br para melhora de habilidades em desenvolvimento web e ganho de conhecimento.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-  return <h1>Olá, meu nome é Enrique e esse será um projeto clone do www.tabnews.com.br</h1>
+  return (
+    <h1>
+      Olá, meu nome é Enrique e esse será um projeto clone do www.tabnews.com.br
+    </h1>
+  );
 }
 
 export default Home;
